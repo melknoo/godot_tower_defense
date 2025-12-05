30473a1269ed780adb4139c95cb39f46299e96fb
melknoo <melle17@gmx.de>
2025-12-05 11:39:14 +0100

    removed tiles

--- ground_layer.gd
+++ ground_layer.gd
@@ -19,12 +19,7 @@ func _ready() -> void:
 
 func _load_tiles() -> void:
 	var tile_names: Array[String] = [
-		"grass", "ground",
-		"path_up", "path_down", "path_left", "path_right",
-		"path_corner_left_up", "path_corner_left_down",
-		"path_corner_right_up", "path_corner_right_down",
-		"path_corner_inner_left_up", "path_corner_inner_left_down",
-		"path_corner_inner_right_up", "path_corner_inner_right_down"
+		"grass", "ground"
 	]
 	
 	for tile_name in tile_names:
@@ -73,64 +68,13 @@ func _get_tiles_for_cell(cell: Vector2i) -> Array[String]:
 	var is_path := cell in path_cells
 	var result: Array[String] = []
 	
-	# Nachbarn (ist dort Pfad?)
-	var path_up := Vector2i(cell.x, cell.y - 1) in path_cells
-	var path_down := Vector2i(cell.x, cell.y + 1) in path_cells
-	var path_left := Vector2i(cell.x - 1, cell.y) in path_cells
-	var path_right := Vector2i(cell.x + 1, cell.y) in path_cells
-	var path_up_left := Vector2i(cell.x - 1, cell.y - 1) in path_cells
-	var path_up_right := Vector2i(cell.x + 1, cell.y - 1) in path_cells
-	var path_down_left := Vector2i(cell.x - 1, cell.y + 1) in path_cells
-	var path_down_right := Vector2i(cell.x + 1, cell.y + 1) in path_cells
-	
-	# === PFAD-ZELLE ===
+
 	if is_path:
-		result.append("ground")  # Braunes Tile
+		result.append("ground")  
 		return result
 	
-	# === GRAS-ZELLE ===
+	result.append("grass")
+
 	
-	# Zähle angrenzende Pfad-Zellen
-	var adjacent := int(path_up) + int(path_down) + int(path_left) + int(path_right)
-	
-	# Kein Pfad direkt daneben - prüfe diagonal
-	if adjacent == 0:
-		if path_up_left:
-			result.append("path_corner_right_down")
-		elif path_up_right:
-			result.append("path_corner_left_down")
-		elif path_down_left:
-			result.append("path_corner_right_up")
-		elif path_down_right:
-			result.append("path_corner_left_up")
-		else:
-			result.append("grass")
-		return result
-	
-	# Pfad an zwei Seiten
-	if path_up and path_left:
-		result.append("path_corner_inner_left_up")
-		return result
-	if path_up and path_right:
-		result.append("path_corner_inner_right_up")
-		return result
-	if path_down and path_left:
-		result.append("path_corner_inner_left_down")
-		return result
-	if path_down and path_right:
-		result.append("path_corner_inner_right_down")
-		return result
-	
-	# Pfad an einer Seite - gerade Kante
-	if path_up:
-		result.append("path_up")
-	elif path_down:
-		result.append("path_down")
-	elif path_left:
-		result.append("path_left")
-	elif path_right:
-		result.append("path_right")
-	else:
-		result.append("grass")
 	
 	return result
